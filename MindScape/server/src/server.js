ce005f840635aa4098a63ed98d9dad99a7aa8896
Rajat Maurya <rajatmaurya176@gmail.com>
2026-03-14 15:21:35 +0530

    fix: checkpoint backend serer

--- MindScape/server/src/server.js
+++ MindScape/server/src/server.js
@@ -10,13 +10,15 @@ dotenv.config({ path: fs.existsSync(envPath) ? envPath : path.join(process.cwd()
 const app = require('./app');
 const connectDB = require('./config/db');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    console.log("Midscape server is running");
   });
 }).catch(err => {
     console.error('Failed to connect to database', err);
     process.exit(1);
 });
+
